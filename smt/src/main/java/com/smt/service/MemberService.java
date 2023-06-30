842690b1e74139dea337d9b553678bca843f9525
kyv1216 <kyv1216@naver.com>
2023-07-01 00:28:03 +0900

    add: join유효성체크,login기능 구현

--- smt/src/main/java/com/smt/service/MemberService.java
+++ smt/src/main/java/com/smt/service/MemberService.java
@@ -6,5 +6,10 @@ public interface MemberService {
 
 	//회원가입
 	public void memberJoin(MemberVO member) throws Exception;
-	
+
+	// 아이디 중복 검사
+	public int idCheck(String userid) throws Exception;
+
+	/* 로그인 */
+    public MemberVO memberLogin(MemberVO member) throws Exception;
 }
